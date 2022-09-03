e1897873fa6cc5f11275fab27b0512cfb8ef56a1
XX Pan <panezion@outlook.com>
2022-09-03 14:25:01 +0800

    my first rust proj

---
+++ guessing_name/target/debug/guessing_name.d
@@ -0,0 +1,1 @@
+/home/panez/learn-rs/guessing_name/target/debug/guessing_name: /home/panez/learn-rs/guessing_name/src/main.rs
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/panez/learn-rs/hello_cargo/target/debug/hello_cargo: /home/panez/learn-rs/hello_cargo/src/main.rs
