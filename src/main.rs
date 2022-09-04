9d73e7dc8c2beb9557e2c3cce4bc9649001e5647
XX Pan <panezion@outlook.com>
2022-09-04 14:59:43 +0800

    change dir

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use minigrep::Config;
+use std::env;
+fn main() {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        std::process::exit(1);
+    }
+}
